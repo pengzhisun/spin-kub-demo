f955e450308377419ff2bb7ecde1efd41adde1a9
vincentliu028 <du520990Vl_10218>
2017-08-17 16:42:08 +0800

    change color

--- main.go
+++ main.go
@@ -11,12 +11,11 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Handling %+v\n", r)
 	bs, err := ioutil.ReadFile("/content/index.html")
-	fmt.Print("The repsponse is : %s\n", string(bs))
+	fmt.Printf("The repsponse is : %s\n", string(bs))
 	if err != nil {
 		fmt.Printf("Couldn't read index.html: %v", err)
 		os.Exit(1)
 	}
-
 	io.WriteString(w, string(bs[:]))
 }
 
