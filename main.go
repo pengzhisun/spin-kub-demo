450d304bc52df388c017c7406b35ec94869f4431
Pengzhi Sun <pengzhisun@live.com>
2017-11-15 12:58:00 -0600

    Update main.go

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", index)
-	port := ":8080"
+	port := ":8000"
 	fmt.Printf("Starting to service on port %s\n", port)
 	http.ListenAndServe(port, nil)
 }
